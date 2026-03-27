f2677bdca241364d7a8a257fda688a765359e095
Elijah Kiragu <95278300+eliki-hue@users.noreply.github.com>
2026-03-27 12:16:44 +0300

    created service view, serializer and url

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
      path('api/jobs/', include('jobs.urls')),
      path('api/auth/', include('users.urls')),
+     path('api/', include('services.urls')),
 ]
