11f9cc304a3be2e680438e89f4087b5c2acda453
Elijah Kiragu <95278300+eliki-hue@users.noreply.github.com>
2026-03-27 12:16:44 +0300

    created service view, serializer and url

--- services/views.py
+++ services/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Service
+from .serializers import ServiceSerializer
+
+
+from users.permissions import IsManagerOrOwner
+
+class ServiceViewSet(ModelViewSet):
+    queryset = Service.objects.all().order_by('price')
+    serializer_class = ServiceSerializer
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsManagerOrOwner()]
+        return [IsAuthenticated()]
