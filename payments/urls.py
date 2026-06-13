de5c2f297ca1c267c7a0fc6eab6faedc04d5d7e4
eliki <eliki13720@gmail.com>
2026-06-13 20:39:43 +0300

    added misssing callback url

--- payments/urls.py
+++ payments/urls.py
@@ -1,7 +1,20 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import PaymentViewSet
+
+from .views import (
+    PaymentViewSet,
+    mpesa_callback,
+)
 
 router = DefaultRouter()
-router.register('payments', PaymentViewSet)
+router.register(r'payments', PaymentViewSet, basename='payments')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path(
+        'payments/mpesa-callback/',
+        mpesa_callback,
+        name='mpesa-callback'
+    ),
+]
+
+urlpatterns += router.urls
